6486674a9f90a10c70adb24c6416b26403085806
Vladimir Belitskiy <belitskiy@gmail.com>
2019-06-01 10:17:23 -0400

    Use various data types for testing linked list's SingleNode

--- tests/test_linked_lists.py
+++ tests/test_linked_lists.py
@@ -1,12 +1,32 @@
 from structures.linked_lists import SingleNode
-# from hypothesis import given
-# from hypothesis.strategies import text
+from hypothesis import given
+from hypothesis import strategies as st
 
 
 class TestSingleNode:
-    def test_node_creation(self, value=5):
+
+    repr_and_str_strategies = given(st.one_of(
+        st.text(), st.integers(), st.booleans(),
+        st.iterables(st.integers()), st.lists(st.integers())
+        ))
+
+    @staticmethod
+    def generate_repr_string(value):
+        value = f'{value!r}' if isinstance(value, str) else value
+        return f'SingleNode({value})'
+
+    def test_value_attr_on_creation(self, value=5):
         assert SingleNode(value).value == value
 
-    def test_node_str_descriptor_output(self, value=5):
+    def test_next_attr_on_creation(self):
+        assert SingleNode(5).next is None
+
+    @repr_and_str_strategies
+    def test_repr_descriptor_output(self, value):
         node = SingleNode(value)
-        assert str(node) == 'Node(' + str(value) + ') --> ' + str(None)
+        assert repr(node) == self.generate_repr_string(value)
+
+    @repr_and_str_strategies
+    def test_str_descriptor_output(self, value):
+        node = SingleNode(value)
+        assert str(node) == f'{self.generate_repr_string(value)} --> {None}'
