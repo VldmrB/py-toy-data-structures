730126946952de6b8db0ae29bf8786de6de5f38b
Vladimir Belitskiy <belitskiy@gmail.com>
2019-06-01 10:29:47 -0400

    Add a faster profile to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,7 @@ from hypothesis import settings, Verbosity
 settings.register_profile('trial',
                           # max_examples=100,
                           verbosity=Verbosity.verbose)
-settings.load_profile('trial')
+settings.register_profile('quick',
+                          max_examples=10,
+                          verbosity=Verbosity.verbose)
+settings.load_profile('quick')  # override with a manual command for Travis CI
