d585867f48109f76e788b27b5e87a5c4d9326f28
Vladimir Belitskiy <belitskiy@gmail.com>
2019-06-09 12:01:19 -0400

    Implement mergesort (new list or in-place)

---
+++ structures/mergesort.py
@@ -0,0 +1,86 @@
+def _merge_sort_in_place(lst: list):
+    lst_len = len(lst)
+    if lst_len < 2:
+        return
+
+    lst_sep = lst_len // 2
+    left = lst[:lst_sep]
+    right = lst[lst_sep:]
+    left_len = len(left)
+    right_len = len(right)
+
+    _merge_sort_in_place(left)
+    _merge_sort_in_place(right)
+
+    i, j, k = 0, 0, 0
+
+    while i < left_len and j < right_len:
+        left_val = left[i]
+        right_val = right[j]
+        if left_val < right_val:
+            lst[k] = left_val
+            i += 1
+        else:
+            lst[k] = right_val
+            j += 1
+        k += 1
+        
+    while i < left_len:
+        lst[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < right_len:
+        lst[k] = right[j]
+        j += 1
+        k += 1
+
+
+def _merge_sort_new(lst: list):
+    lst_len = len(lst)
+    if lst_len < 2:
+        return lst
+    
+    sorted_lst = [0] * lst_len
+    lst_sep = lst_len // 2
+    
+    left = lst[:lst_sep]
+    right = lst[lst_sep:]
+    left_len = len(left)
+    right_len = len(right)
+
+    left_sorted = _merge_sort_new(lst[:lst_sep])
+    right_sorted = _merge_sort_new(lst[lst_sep:])
+    
+    i, j, k = 0, 0, 0
+    
+    while i < left_len and j < right_len:
+        left_val = left_sorted[i]
+        right_val = right_sorted[j]
+
+        if left_val < right_val:
+            sorted_lst[k] = left_val
+            i += 1
+        else:
+            sorted_lst[k] = right_val
+            j += 1
+        k += 1
+    
+    while i < left_len:
+        sorted_lst[k] = left_sorted[i]
+        i += 1
+        k += 1
+    
+    while j < right_len:
+        sorted_lst[k] = right_sorted[j]
+        j += 1
+        k += 1
+    
+    return sorted_lst
+
+
+def merge_sort(lst: list, in_place=False):
+    if in_place:
+        _merge_sort_in_place(lst)
+    else:
+        return _merge_sort_new(lst)
