48aab9ca9fd193efa8425e5bd43ba3171abf3b4b
Vladimir Belitskiy <belitskiy@gmail.com>
2019-07-01 13:59:41 -0400

    - Add tests for the mergesort algorithm  - Add .coveragerc; add some of its products to .gitignore  - Remove the option for showing hypothesis stats during test runs, from pytest.ini

---
+++ tests/test_algorithms.py
@@ -0,0 +1,25 @@
+from copy import deepcopy
+
+from hypothesis import strategies as st, given
+
+from algorithms.mergesort import merge_sort
+
+
+class TestMergesort:
+
+    int_str_lists_and_list_of_lists = given(
+        st.one_of(st.lists(st.integers()),
+                  st.lists(st.text()),
+                  st.lists(st.lists(st.integers()), 1),
+                  st.lists(st.lists(st.text()), 1)
+                  ))
+
+    @int_str_lists_and_list_of_lists
+    def test_mergesort_in_place(self, value):
+        copied = deepcopy(value)
+        merge_sort(value, in_place=True)
+        assert value == sorted(copied)
+
+    @int_str_lists_and_list_of_lists
+    def test_mergesort_new(self, value):
+        assert merge_sort(value, in_place=False) == sorted(value)
